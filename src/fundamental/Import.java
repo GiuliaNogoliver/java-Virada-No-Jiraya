371a0e6a9ea46f59887fa6581c55c1a3a1b468b0
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-02 18:46:51 -0300

    Imports

---
+++ src/fundamental/Import.java
@@ -0,0 +1,20 @@
+package fundamental;
+
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+	public static void main(String[] args) {
+		String a = "Bom dia!";
+		System.out.println(a);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		JButton botao = new JButton();
+
+		/*java.lang.String b = "Exemplo"
+		java.lang.System.out.printl(b);*/
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module javaViradaNoJiraya {
+	requires java.desktop;
 }

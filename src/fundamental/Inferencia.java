40e6eea092c456f04ff0367e437eb91c560086d0
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-02 16:28:52 -0300

    Inferencia

--- src/fundamental/Inferencia.java
+++ src/fundamental/Inferencia.java
@@ -5,9 +5,17 @@ public class Inferencia {
 		/*o uso do var significa q vc n esta declarando o
 		 * tipo de variavel mas logo qnd vc fala qual dado
 		 * vai receber ele automaticamente atribui o tipo
-		 */
+		   ou seja ira inferir o tipo de dado*/
 		var a = 1.0;
+		System.out.println(a);
 		
+		//note nao posso agr dar uma variavel texto ao a
+		//a = "Texto";
 		
+		a = 23.9;
+		System.out.println(a);
+		
+		var b = "Texto";
+		System.out.println(b);
 	}
 }

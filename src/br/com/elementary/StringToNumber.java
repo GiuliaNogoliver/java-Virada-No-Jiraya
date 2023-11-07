c99fb841a387cb8fc32553dcbba762cfc1d7799c
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-07 13:29:12 -0300

    String to number using wrapper

---
+++ src/br/com/elementary/StringToNumber.java
@@ -0,0 +1,23 @@
+package br.com.elementary;
+
+import javax.swing.JOptionPane;
+
+public class StringToNumber {
+	public static void main(String[] args) {
+		String value01 = JOptionPane.showInputDialog(
+				"Type the first number");
+		
+		String value02 = JOptionPane.showInputDialog(
+				"Type the second number");
+		
+		System.out.println(value01 + value02);
+		
+		double number1 = Double.parseDouble(value01);
+		double number2 = Double.parseDouble(value02);
+		
+		double soma = number1 + number2;
+		System.out.println("A soma é " + soma);
+		double media = soma/2;
+		System.out.println("A media é " + media);
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,3 @@
-/**
- * 
- */
-/**
- * 
- */
 module javaNoJiraya {
+	requires java.desktop;
 }

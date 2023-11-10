387063c378d0dc59ed774445581cb24ef87c62be
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-09 23:50:52 -0300

    Using and understand better assigments

--- src/br/com/elementary/Assignment.java
+++ src/br/com/elementary/Assignment.java
@@ -3,16 +3,19 @@ package br.com.elementary;
 public class Assignment {
 	public static void main(String[] args) {
 		
+		int ab;
 		int a = 3;
 		int b = a;
 		int c = a + b;
+		
+		ab = c++;
 		
 		c += b; //c = c + b;
 		c -= a; //c = c - a;
 		c *= b; //c = c * b;
 		c /= b; //c = c / b:
 		
-		System.out.println(c);
+		System.out.println(c + ab);
 		
 		c%= 2; //c = c % 2; 0 ou 1
 		System.out.println(c);

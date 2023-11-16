b68981a5b692a0803a8d58b0eb1be361fc432399
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-16 17:47:59 -0300

    using while

---
+++ src/br/com/ControlStructures/DeterminedWhile.java
@@ -0,0 +1,12 @@
+package br.com.controlStructures;
+
+public class DeterminedWhile {
+	public static void main(String[] args) {
+		int contador = 1;
+		
+		while(contador < 10) {
+			System.out.println("contador baixo");
+			contador++;
+		}
+	}
+}

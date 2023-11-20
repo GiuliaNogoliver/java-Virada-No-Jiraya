faa05de2b264b3a4305fcd53ce645b65d3247340
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-19 22:02:45 -0300

    for Challenge

---
+++ src/br/com/ControlStructures/ChallengeFor.java
@@ -0,0 +1,9 @@
+package br.com.controlStructures;
+
+public class ChallengeFor {
+	public static void main(String[] args) {
+		for(String i = "#";!i.equals("######"); i += "#") {
+			System.out.println(i);
+		}
+	}
+}

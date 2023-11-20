d4cc01b7c17dfce6a5ddc81063796f4cbfd05f02
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-20 14:25:54 -0300

    continue label

---
+++ src/br/com/ControlStructures/ContinueLabel.java
@@ -0,0 +1,15 @@
+package br.com.controlStructures;
+
+public class ContinueLabel {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			interno: for (int j = 0; j < 10; j++) {
+				System.out.printf("[%d %d]", i, j);
+				if (i < 7) {
+					continue interno;
+				}
+			}
+			System.out.println();
+		}
+	}
+}

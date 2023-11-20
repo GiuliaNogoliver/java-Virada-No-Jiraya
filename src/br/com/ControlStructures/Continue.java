d31e2c9e50b52709c8bfd3cb003783278383e326
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-20 13:43:23 -0300

    using continue

---
+++ src/br/com/ControlStructures/Continue.java
@@ -0,0 +1,13 @@
+package br.com.controlStructures;
+
+public class Continue {
+	public static void main(String[] args) {
+		for(int i = 10;i > 0; i--) {
+			System.out.println(i);
+			if(i >= 5) {
+				System.out.println(i);
+				continue;
+			}
+		}
+	}
+}

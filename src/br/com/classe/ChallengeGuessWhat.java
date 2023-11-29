f422a22b0811f64f4650c50c8eddfea60a28f59f
Giulianogoliver <giulianogoliver84@gmail.com>
2023-11-29 17:42:09 -0300

    logic challenge

---
+++ src/br/com/classe/ChallengeGuessWhat.java
@@ -0,0 +1,15 @@
+package br.com.classe;
+
+public class ChallengeGuessWhat {
+
+	int a = 2;
+	static int b = 3;
+	
+	public static void main(String[] args) {
+
+		ChallengeGuessWhat b = new ChallengeGuessWhat();
+		System.out.println(b.a);
+		
+		System.out.println(b);
+	}
+}

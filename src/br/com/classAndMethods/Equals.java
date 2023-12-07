4e2793f9c90a69a76d2d4bc21de7e44129f98943
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-07 20:21:52 -0300

    modify equals and keep instanceof

--- src/br/com/classAndMethods/Equals.java
+++ src/br/com/classAndMethods/Equals.java
@@ -5,13 +5,18 @@ public class Equals {
 		User user01 = new User();
 		user01.name = "Wellington";
 		user01.email = "wellpereira@uorak.com";
-		
+
 		User user02 = new User();
+		user01.name = "Wellington";
+		user01.email = "wellpereira@uorak.com";
+		
+		User user03 = new User();
 		user01.name = "Giulia";
 		user01.email = "giuliaoliveira@uorak.com";
 		
 		System.out.println(user01.equals(user02));
 		System.out.println(user01 == user02);
+		System.out.println(user02.equals(user03));
 		
 	}
 }
--- src/br/com/classAndMethods/User.java
+++ src/br/com/classAndMethods/User.java
@@ -3,10 +3,21 @@ package br.com.classAndMethods;
 public class User {
 	String name;
 	String email;
-	
+
 	public boolean equals(Object objeto) {
-		User other = (User) objeto;
+//		User other = (User) objeto;
+
+		if(objeto instanceof User) {
+			return true;
+		}
+		return false;
 		
-		return super.equals(objeto);
+//		boolean equalName = other.name.equals(this.name);
+//		boolean equalEmail = other.email.equals(this.email);
+		
+//      if(other.name == this.name && other.email == this.email) {
+//			return true;
+//		}
+//		return equalName && equalEmail;
 	}
 }

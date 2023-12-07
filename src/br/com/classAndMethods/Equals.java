f1964a22449b8c16198d55c50a3c491e7e0ebb85
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-06 21:59:26 -0300

    exploring equals

---
+++ src/br/com/classAndMethods/Equals.java
@@ -0,0 +1,17 @@
+package br.com.classAndMethods;
+
+public class Equals {
+	public static void main(String[] args) {
+		User user01 = new User();
+		user01.name = "Wellington";
+		user01.email = "wellpereira@uorak.com";
+		
+		User user02 = new User();
+		user01.name = "Giulia";
+		user01.email = "giuliaoliveira@uorak.com";
+		
+		System.out.println(user01.equals(user02));
+		System.out.println(user01 == user02);
+		
+	}
+}
---
+++ src/br/com/classAndMethods/User.java
@@ -0,0 +1,12 @@
+package br.com.classAndMethods;
+
+public class User {
+	String name;
+	String email;
+	
+	public boolean equals(Object objeto) {
+		User other = (User) objeto;
+		
+		return super.equals(objeto);
+	}
+}

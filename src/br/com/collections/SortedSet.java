d754d06f0d0b29518c0c28197bc8a481c9c19953
Giulianogoliver <giulianogoliver84@gmail.com>
2023-12-08 16:54:54 -0300

    creating a set with the same type

---
+++ src/br/com/collections/SortedSet.java
@@ -0,0 +1,32 @@
+package br.com.collections;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class SortedSet {
+	public static void main(String[] args) {
+		//<> isso é o generics, vc define o tipo de dado q recebera
+		TreeSet<Object> list = new TreeSet<Object>();
+		list.add("Giulia");
+		list.add("Wellington");
+		list.add("Ana");
+		list.add("Victor");
+		list.add("Matheus");
+		
+		System.out.println(list);
+		for(Object name: list) {
+		System.out.println(name);
+		}
+		
+		HashSet<Integer> numbers = new HashSet<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		
+		System.out.println(numbers);
+		for(int nums: numbers) {
+			System.out.println(nums);
+		}
+	}
+}

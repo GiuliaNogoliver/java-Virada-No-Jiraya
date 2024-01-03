070b14178222837cb3cac40a18dfad0880928c14
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-03 16:16:31 -0300

    annotation for interface lambda

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,5 +1,11 @@
 package lambdas;
 
+// Quando colocamos essa anotacao significa que esta interface 
+// nao pode ter outros metodos, somente esse
+// Normalmente usado para interface de lambdas
+
+@FunctionalInterface
 public interface Calculo {
 	double calcular(double a, double b);
+	//int teste();  Note q se descomentar este metodo a classe nao funciona mais
 }

7d91dcbc24cdc57844dfdfc5fefef462d93d9106
Giulianogoliver <giulianogoliver84@gmail.com>
2024-01-04 17:49:55 -0300

    understanding better Lambda and Method Reference

--- src/lambdas/Foreach.java
+++ src/lambdas/Foreach.java
@@ -11,12 +11,22 @@ public class Foreach {
         	System.out.println(nome);
         }
         
-        System.out.println("\nUsando Lambda: ");
+        System.out.println("\nUsando Lambda #1: ");
         //aprovados.forEach((nome) -> {System.out.println(nome);});
-        aprovados.forEach(nome -> System.out.println(nome));
+        aprovados.forEach(nome -> System.out.println(nome + "!!!"));
         
-        // Quando menos codigo masi dificil de entender mas mais rapido de fazer
-        System.out.println("\nMethod Reference: ");
+        // Quando menos codigo mais dificil de entender mas mais rapido de fazer
+        System.out.println("\nMethod Reference #1: ");
         aprovados.forEach(System.out::println);
+        
+        System.out.println("\nUsando Lambda #2: ");
+        aprovados.forEach(nome -> imprimir(nome));
+        
+        System.out.println("\nMethod Reference #2: ");
+        aprovados.forEach(Foreach::imprimir);
+    }
+    
+    static void imprimir(String nome) {
+    	System.out.println("nome: " + nome);
     }
 }

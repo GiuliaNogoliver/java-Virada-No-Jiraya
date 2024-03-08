f04df2a3fabe3111a94ac5eb3a2ede2f9f4950d3
Giulianogoliver <giulianogoliver84@gmail.com>
2024-03-08 20:13:02 -0300

    java swing intro, buttons and layout define

--- src/swing/Observador.java
+++ src/swing/Observador.java
@@ -1,5 +1,8 @@
 package swing;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Observador {
@@ -7,6 +10,15 @@ public class Observador {
 		JFrame janela = new JFrame("Observador");
 		janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		janela.setSize(600, 200);
+		// deixar o botao em cima e nao ocupar a tela toda
+		janela.setLayout(new FlowLayout());
+		// centraliza no centro da tela do computador
+		janela.setLocationRelativeTo(null);
+		
+		JButton botao = new JButton("Clicar!");
+		janela.add(botao);
+
+		botao.addActionListener(e->{System.out.println("Evento ocorreu!");});
 		
 		janela.setVisible(true);
 	}
